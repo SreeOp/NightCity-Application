c55bf190720e6e7e201b181762c7528a6568c4d7
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-12-11 13:52:48 +0530

    whitelist.js

--- whitelist.js
+++ whitelist.js
@@ -29,12 +29,12 @@ module.exports = async (client) => {
           applyChannel.send({
             embeds: [
               new EmbedBuilder()
-                .setTitle(`DHRUVAM APPLICATIONS`)
+                .setTitle(`NIGHTCITY APPLICATIONS`)
                 .setAuthor({ name: `${process.env.ServerName} | Server Application`, iconURL: process.env.ServerLogo })
-                .setColor("#00e5ff")
+                .setColor("#b100cd")
                 .setThumbnail("https://ik.imagekit.io/yg4xrysan/RageGaming/Dhruvam/BOT/123444d_43EFIfH9Q-.webp")
                 .setImage('https://ik.imagekit.io/yg4xrysan/RageGaming/Dhruvam/BOT/DR3P1234j_TqGJUBxWjy.webp')
-                .setFooter({ text: "Test" ? `${process.env.ServerName}` : `${process.env.ServerName}` })
+                .setFooter({ text: "NRP" ? `${process.env.ServerName}` : `${process.env.ServerName}` })
                 .setTimestamp(new Date())
             ],
             components: [btnrow],
